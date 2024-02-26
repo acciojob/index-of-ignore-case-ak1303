ffb4c957ae18ba1053007b4b6b4cab9cd22ca227
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-02-26 22:35:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-return s1.toLowerCase().split(s2.toLowerCase())[0].length;
+	
+let ans = s1.toLowerCase().split(s2.toLowerCase())[0].length;
+  return ans==s1.length?-1:ans; 
 }
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+alert(indexOfIgnoreCase(s1, s2)); 
